97e01aed2fdf4dc78f6f8be7da65ef85f3043187
Weldo Carvalho <weldocarvalho@outlook.com>
2022-04-27 17:32:48 -0300

    refactor: change app.use(router) from middlewares to app/index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import setupMiddlewares from './middlewares'
+import { router } from './routes'
 dotenv.config()
 
 const app = express()
@@ -9,3 +10,4 @@ app.listen(process.env.APP_PORT, () => {
 })
 
 setupMiddlewares(app)
+app.use(router)
--- src/app/middlewares/index.ts
+++ src/app/middlewares/index.ts
@@ -1,10 +1,8 @@
 import express, { Express } from 'express'
-import { router } from '../routes'
 
 const setupMiddlewares = (app: Express): void => {
 	app.use(express.json())
 	app.use(express.urlencoded({ extended: true }))
-	app.use(router)
 }
 
 export default setupMiddlewares
