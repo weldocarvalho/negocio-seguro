d0e000d49a636af5a2a4bf0c6fdc6148ecde4d2f
Weldo Carvalho <weldocarvalho@outlook.com>
2022-04-30 16:02:20 -0300

    #5 #3 chore/refactor: include validator and refator

--- src/app/controllers/signup.controller.ts
+++ src/app/controllers/signup.controller.ts
@@ -1,17 +1,27 @@
 import { createUser } from '../../business/services/user.service'
+import { emailValidator } from '../../utils/validator'
 import { IController } from '../protocols/controller.protocol'
 import { IHttpRequest, IHttpResponse } from '../protocols/http.protocol'
 
 export const signupController: IController = {
 	handle: async (req: IHttpRequest): Promise<IHttpResponse> => {
 		const requiredFields = ['name', 'email', 'password']
+		const errors: string[] = []
 
 		for (const field of requiredFields) {
 			if (!req.body[field]) {
-				return {
-					statusCode: 400,
-					body: `Missing param error: ${field}`,
-				}
+				errors.push(`Missing param error: ${field}`)
+			}
+		}
+
+		if (!emailValidator(req.body.email)) {
+			errors.push('Invalid email')
+		}
+
+		if (errors.length > 0) {
+			return {
+				statusCode: 400,
+				body: errors,
 			}
 		}
 
