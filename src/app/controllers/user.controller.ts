2a38cd8ffaa3f435d824a38f6badbde68e41b8b6
Weldo Carvalho <weldocarvalho@outlook.com>
2022-04-30 22:29:30 -0300

    fix (userController): change argument to search user

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express'
 import { findOne, createUser } from '../../business/services/user.service'
 
+// TODO: implements route adapter for this controller also
 const userController = {
 	create: async (req: Request, res: Response) => {
 		const user = await createUser(req.body)
 		return res.status(200).json({ user })
 	},
+	// TODO: update to methods findById, findByEmail...
 	find: async (req: Request, res: Response) => {
-		const { userId } = req.body
-		const user = await findOne(userId)
+		const { email } = req.body
+		const user = await findOne(email)
 		return res.status(200).json({ user })
 	},
 }
