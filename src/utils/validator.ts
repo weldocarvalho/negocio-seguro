22fac9bce1a0fa3a55d86a0b0317f739eff7cac5
Weldo Carvalho <weldocarvalho@outlook.com>
2022-05-09 15:09:30 -0300

    feat: add isMobilePhone validator

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,3 +3,7 @@ import validator from 'validator'
 export const emailValidator = (email: string): boolean => {
 	return validator.isEmail(email)
 }
+
+export const mobilePhoneValidator = (phone: string) => {
+	return validator.isMobilePhone(phone)
+}
