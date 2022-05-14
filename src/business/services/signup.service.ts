ca3d1733b05e0da09785bdc7340585df19629ed4
Weldo Carvalho <weldocarvalho@outlook.com>
2022-05-13 22:57:41 -0300

    chore (signup.service): remove mobile validator, add named params, include signinService in signup flow

--- src/business/services/signup.service.ts
+++ src/business/services/signup.service.ts
@@ -1,17 +1,14 @@
-import { INVALID_EMAIL, INVALID_MOBILE_PHONE } from '../../app/errors/errorTypes'
-import { emailValidator, mobilePhoneValidator } from '../../utils/validator'
+import { INVALID_EMAIL } from '../../app/errors/errorTypes'
+import { hashPassword } from '../../utils/encrypter'
+import { emailValidator } from '../../utils/validator'
+import { signinService } from './signin.service'
 import { createUser } from './user.service'
 
-const signupService = async (email: string, password: string, phone: string) => {
+const signupService = async (email: string, password: string) => {
 	const errors: string[] = []
 
 	if (!emailValidator(email)) {
 		errors.push(INVALID_EMAIL)
-	}
-
-	// TODO: improve phone number validation (add locales)
-	if (!mobilePhoneValidator(phone)) {
-		errors.push(INVALID_MOBILE_PHONE)
 	}
 
 	if (errors.length > 0) {
@@ -22,16 +19,11 @@ const signupService = async (email: string, password: string, phone: string) => 
 	}
 
 	const [name] = email.split('@')
-
-	const userCredentials = {
-		name,
-		email,
-		password,
-		phone,
-	}
+	const hashedPassword = hashPassword(password)
 
 	try {
-		await createUser(userCredentials)
+		await createUser({ name, email, hashedPassword })
+		return await signinService(email, password, true)
 	} catch (error: any) {
 		console.error(error)
 		throw error
